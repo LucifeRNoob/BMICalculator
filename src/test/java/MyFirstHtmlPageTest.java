b504969b5425192affd8ebc2d36f4dfb15ec8769
LucifeRNoob <andrewkremenevskiy@gmail.com>
2022-01-31 21:11:04 +0300

    MyFirstHtmlPageTest update

--- src/test/java/MyFirstHtmlPageTest.java
+++ src/test/java/MyFirstHtmlPageTest.java
@@ -2,29 +2,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class MyFirstHtmlPageTest {
 
-    private static final String path = "file:/c:/2/BMICalculator/src/test/resources/index.html";
+    private static final String PATH = "file:/c:/2/BMICalculator/src/test/resources/index.html";
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void visitorGreetingTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         //Open My first html page
-        driver.get(path);
+        driver.get(PATH);
         //Check element header1
         Assert.assertEquals(driver.findElement(By.id("h1")).getText(), "Welcome, stranger!");
-        driver.quit();
     }
 
     @Test
     public void bandListTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         //Open My first html page
-        driver.get(path);
+        driver.get(PATH);
         //Check element label1
         Assert.assertEquals(driver.findElement(By.id("l1")).getText(), "My favorite bands:");
         //Click on Band list
@@ -37,53 +39,45 @@ public class MyFirstHtmlPageTest {
         Assert.assertEquals(driver.findElement(By.id("band3")).getText(), "Scorpions");
         //Check element Black Sabbath
         Assert.assertEquals(driver.findElement(By.id("band4")).getText(), "Black Sabbath");
-        driver.quit();
     }
 
     @Test
     public void alertButtonTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         //Open My first html page
-        driver.get(path);
+        driver.get(PATH);
         //Check 'Click me' button
         boolean isClickMeButtonIsDisplayed = driver.findElement(By.cssSelector("#alert")).isDisplayed();
         Assert.assertTrue(isClickMeButtonIsDisplayed, "Click me isn't show");
-        driver.quit();
     }
 
     @Test
     public void containNameTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         //Open My first html page
-        driver.get(path);
+        driver.get(PATH);
         //Is the name Andrew in the string
-        Assert.assertEquals(driver.findElement(By.id("p")).getText().contains("Andrew"), true);
-        driver.quit();
+        Assert.assertTrue(driver.findElement(By.id("p")).getText().contains("Andrew"));
     }
 
     @Test
     public void resourceOpenedTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         //Open My first html page
-        driver.get(path);
+        driver.get(PATH);
         //Сlick on the link
         driver.findElement(By.id("hyper")).click();
         //Check that 'Onliner' has opened
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.onliner.by/");
-        driver.quit();
     }
 
     @Test
     public void imageHasAlternativeText() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         //Open My first html page
-        driver.get(path);
+        driver.get(PATH);
         //Check text 'oops, you found it'
         Assert.assertEquals(driver.findElement(By.id("pic")).getAttribute("alt"), "oops, you found it");
-        driver.close();
+    }
+
+    @AfterMethod
+    public void closeDriver(){
+        driver.quit();
     }
 }
